69924de9a84141df0ed32fac755813efed09d10c
Sebastian Ibarguen <s@cognits.co>
2014-12-15 17:07:40 -0600

    Have basic working examples of components.

---
+++ js/controls.js
@@ -0,0 +1,10 @@
+var viewModel = {
+  stringValue : ko.observable("Hello"),
+  passwordValue : ko.observable("mypass"),
+  booleanValue : ko.observable(true),
+  optionValues : ["Alpha", "Beta", "Gamma"],
+  selectedOptionValue : ko.observable("Gamma"),
+  multipleSelectedOptionValues : ko.observable(["Alpha"]),
+  radioSelectedOptionValue : ko.observable("Beta")
+};
+ko.applyBindings(viewModel);
